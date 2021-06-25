718c67a508be82911bf5e1fe8c9a3ec1069dbec3
Sergey Milaev <milaevsergey@gmail.com>
2021-06-25 21:57:46 +0300

    auto-commit for deploy

---
+++ precache-manifest.dfe9f0583fc1ea8e05307ae6770622b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d70dfdd92e6ae4fb56b4b49e480e32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "666cdd02cb20771be41c",
+    "url": "/static/css/main.626a73d8.chunk.css"
+  },
+  {
+    "revision": "d11f4a7af2032037725a",
+    "url": "/static/js/2.fded9e56.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.fded9e56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "666cdd02cb20771be41c",
+    "url": "/static/js/main.ae54f041.chunk.js"
+  },
+  {
+    "revision": "70b87a81e80f2bb3be62",
+    "url": "/static/js/runtime-main.d1934821.js"
+  }
+]);
